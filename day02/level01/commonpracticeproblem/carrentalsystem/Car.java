74ab647f9c32407f1f1c62638749eaacbdc8d3e3
Vishal Jadhav <129534967+vishaljadhaw@users.noreply.github.com>
2025-03-02 21:00:40 +0530

    Delete day02/level01 directory

--- day02/level01/commonpracticeproblem/carrentalsystem/Car.java
+++
@@ -1,9 +0,0 @@
-package day02.level01.commonpracticeproblem.carrentalsystem;
-
-public class Car {
-    public static void main(String[] args) {
-        CarRentalSystem carRental = new CarRentalSystem("John Doe", "Toyota", 5);
-        System.out.println("Total Cost: $" + carRental.calculateTotalCost());
-    }
-
-}
